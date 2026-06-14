b938fe2c5b696e9681371332054bd379c77ee9ef
MSeifoori <mseifoori@outlook.com>
2026-06-14 19:40:21 +0330

    Creating bacteria datasets

---
+++ data/bacteria_lists.py
@@ -0,0 +1,47 @@
+enterobacteriaceae_bacteria = [
+    'Normal E.coli',
+    'EHEC',
+    'EIEC',
+    'Shigella',
+    'Klebsiella pneumoniae',
+    'Klebsiella oxytoca',
+    'Enterobacter',
+    'Serratia',
+    'Proteus mirabilis',
+    'Proteus vulgaris',
+    'Providencia',
+    'Morganella',
+    'Salmonella typhi',
+    'Salmonella paratyphi A',
+    'Salmonella paratyphi other groups',
+    'Salmonella arizona',
+    'Edwardsiella'
+    'Citrobacter freundii',
+    'Citrobacter diversus'
+]
+
+nfb_bacteria = [
+    'Pseudomonas aeruginosa',
+    'Alcaligenes faecalis',
+    'Cryseobacterium meingosepticum',
+    'Moraxella catarrhalis',
+    'Moraxella lacunata',
+    'Shewanella putrefaciens',
+    'Acinetobacter baumannii',
+    'Acintobacter lwoffii',
+    'Borkholderia cepacia',
+    'Stenotrophomonas maltophilia',
+]
+
+gram_positive_bacteria = [
+    'Micrococcus',
+    'Staphylococcus areus',
+    'Staphylococcus epidermidis',
+    'Staphylococcus saprophyticus',
+    'Streptococcus pyogenes (group A)',
+    'Streptococcus agalactiae (group B)',
+    'Streptococcus pneumoniae',
+    'Streptococcus viridians',
+    'Enterococcus',
+    'Non-Enterococcus'
+]
